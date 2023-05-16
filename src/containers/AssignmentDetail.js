492b70bc51f6ce276954704b2297c45008f435be
Akash Verma <akash.911verma@gmail.com>
2023-05-16 23:39:46 +0530

    first commit

---
+++ src/containers/AssignmentDetail.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react'
+import { connect } from "react-redux";
+import { Card, Skeleton, message } from 'antd';
+import 'antd/dist/antd.css';
+import Questions from './Questions';
+import { getASNTSDetail } from "../store/actions/assignments";
+import Hoc from "../hoc/hoc"
+import Choices from '../components/Choices';
+import { createGradedASNT } from "../store/actions/gradedAssignments";
+
+const cardStyle = {
+    marginTop: "20px",
+    marginBottom: "20px"
+};
+
+export class AssignmentDetail extends Component {
+
+    state = {
+        usersAnswers: {}
+    };
+
+    componentDidMount() {
+        if (this.props.token !== undefined && this.props.token !== null) {
+            this.props.getASNTSDetail(this.props.token, this.props.match.params.id);
+        }
+    }
+
+    componentWillReceiveProps(newProps) {
+        if (newProps.token !== this.props.token) {
+            if (newProps.token !== undefined && newProps.token !== null) {
+                this.props.getASNTSDetail(newProps.token, this.props.match.params.id);
+            }
+        }
+    }
+
+    onChange = (e, qId) => {
+        const { usersAnswers } = this.state;
+        usersAnswers[qId] = e.target.value;
+        this.setState({ usersAnswers });
+    };
+
+    handleSubmit() {
+        message.success("Submitting your assignment!");
+        const { usersAnswers } = this.state;
+        const asnt = {
+            username: this.props.username,
+            asntId: this.props.currentAssignment.id,
+            answers: usersAnswers
+        };
+        console.log(asnt)
+        this.props.createGradedASNT(this.props.token, asnt);
+    }
+
+    render() {
+        const { currentAssignment } = this.props;
+        const { title } = currentAssignment;
+        const { usersAnswers } = this.state
+        console.log(currentAssignment)
+        return (
+            <Hoc>
+                {Object.keys(currentAssignment).length > 0 ? (
+                <Hoc>
+                    {
+                        this.props.loading ? (
+                            <Skeleton active />
+                        ) : (
+                            <Card title={title}>
+                                <Questions 
+                                    submit={() => this.handleSubmit()}
+                                    questions={currentAssignment.questions.map(q => {
+                                    return <Card
+                                        style={cardStyle}
+                                        type="inner"
+                                        key={q.id}
+                                        title={`${q.order}. ${q.question}`}
+                                    >
+                                        <Choices
+                                            questionId={q.order}
+                                            choices={q.choices}
+                                            change={this.onChange}
+                                            usersAnswers={usersAnswers}
+                                        />
+                                    </Card>
+                                })} />
+                            </Card>
+                        )
+                    }
+                </Hoc>
+                ) : <h1>Empty Assignment</h1>}
+            </Hoc>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        token: state.auth.token,
+        currentAssignment: state.assignments.currentAssignment,
+        loading: state.assignments.loading,
+        username: state.auth.username
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getASNTSDetail: (token, id) => dispatch(getASNTSDetail(token, id)),
+        createGradedASNT: (token, asnt) => dispatch(createGradedASNT(token, asnt))
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(AssignmentDetail);
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore, compose, applyMiddleware, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import authReducer from "./store/reducers/auth";
+import assignmentReducer from "./store/reducers/assignments";
+import gradedAssignmentReducer from "./store/reducers/gradedAssignments";
+
+
+const composeEnhances = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+
+const rootReducer = combineReducers({
+   auth: authReducer,
+   assignments: assignmentReducer,
+   gradedAssignments: gradedAssignmentReducer
+});
+
+
+const store = createStore(rootReducer, composeEnhances(applyMiddleware(thunk)));
+
+const app = (
+   <Provider store={store}>
+      <App />
+   </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
+registerServiceWorker();
